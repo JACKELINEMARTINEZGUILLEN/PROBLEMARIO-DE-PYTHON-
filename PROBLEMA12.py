23aa35e01573c6f11ad4c6774cbb0c4f1436a58b
JACKELINEMARTINEZGUILLEN <l24230212@minatitlan.tecnm.mx>
2024-11-25 19:56:21 -0600

    Update PROBLEMA12.py

--- PROBLEMA12.py
+++ PROBLEMA12.py
@@ -6,4 +6,19 @@ import numpy as np
 def gauss_eliminacion(A, b):
     n = len(b)
     Ab = np.hstack([A, b.reshape(-1, 1)])
-  
+    
+    for i in range(n):
+
+        max_row = np.argmax(np.abs(Ab[i:, i])) + i
+        Ab[[i, max_row]] = Ab[[max_row, i]]
+        
+        Ab[i] = Ab[i] / Ab[i, i]
+        
+        for j in range(i + 1, n):
+            Ab[j] = Ab[j] - Ab[i] * Ab[j, i]
+
+     x = np.zeros(n)
+     for i in range(n - 1, -1, -1):
+         x[i] = Ab[i, -1] - np.dot(Ab[i, i + 1:n], x[i + 1:n])
+    
+     return x 
