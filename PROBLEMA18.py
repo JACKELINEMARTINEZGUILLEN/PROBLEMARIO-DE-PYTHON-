ba1092fd78514049be1900896c06fc6222286ef6
JACKELINEMARTINEZGUILLEN <l24230212@minatitlan.tecnm.mx>
2024-11-25 20:19:12 -0600

    Create PROBLEMA18.py

---
+++ PROBLEMA18.py
@@ -0,0 +1,23 @@
+#Factorización optimizada: Diseña una función para descomponer un 
+#número en sus factores primos de forma eficiente.
+
+def factorizacion_prima(n):
+    factores = defaultdict(int)
+    while n % 2 == 0:
+        factores[2] += 1
+        n //= 2
+    while n % 3 == 0:
+        factores[3] += 1
+        n //= 3
+    for i in range(5, int(math.sqrt(n)) + 1, 6):
+        while n % i == 0:
+            factores[i] += 1
+            n //= i
+        while n % (i + 2) == 0:
+            factores[i + 2] += 1
+            n //= (i + 2)
+
+    if n > 1:
+        factores[n] += 1 
+    
+    return dict(factores)
