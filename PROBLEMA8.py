5d7e0094dd4dd78bac7e98ed3c28fda6956d231c
JACKELINEMARTINEZGUILLEN <l24230212@minatitlan.tecnm.mx>
2024-11-25 19:38:49 -0600

    Update PROBLEMA8.py

--- PROBLEMA8.py
+++ PROBLEMA8.py
@@ -1,4 +1,5 @@
-#Aproximación de derivadas usando diferencias finitas
+#Aproximación de derivadas: Implementa una función que calcule la 
+#derivada de una función en un punto usando diferencias finitas.
 
 def derivada_numerica(f, x, h=1e-5):
     return (f(x + h) - f(x - h)) / (2 * h)
