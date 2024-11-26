f329c03391f49a260a65090d08d476b28ccca8f5
JACKELINEMARTINEZGUILLEN <l24230212@minatitlan.tecnm.mx>
2024-11-25 19:07:20 -0600

    Create PROBLEMA4.py

---
+++ PROBLEMA4.py
@@ -0,0 +1,18 @@
+#Fibonacci y primos: Encuentra el número más cercano a un índice N en 
+#la serie de Fibonacci que también sea primo.
+
+def es_primo(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def fibonacci_primo_cercano(N):
